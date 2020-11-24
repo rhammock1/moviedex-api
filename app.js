3784a63bed6d2bb7bf7741243124fbb02a6e121d
rhammock1 <rhamm1027@gmail.com>
2020-11-24 15:22:10 -0500

    Added getAllMovies handler

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const helmet = require('helmet')
 const cors = require('cors');
 const morgan = require('morgan');
 const app = express();
+const movies = require('./moviedex.js');
 
-app.use(cors())
+app.use(cors());
 app.use(morgan("combined"));
-app.use(helmet())
+app.use(helmet());
 
 const API_TOKEN = process.env.API_TOKEN;
-const validateBearerToken = () => {
+const validateBearerToken = function(req, res, next) {
   const authVal = req.get('Authorization') || '';
   if(!authVal.startsWith('Bearer ')) {
     return res.status(400).json({message: 'Must include Bearer token'})
@@ -22,10 +23,18 @@ const validateBearerToken = () => {
   next();
 }
 app.use(validateBearerToken)
-app.get('/movies', (req, res) => {
 
-})
+const handleGetAllMovies = function(req, res) {
+  res.json({ movies });
+}
+const handleGetMoviesByGenre = function(req, res) {
+  req.query.genre
+}
 
+const handleGetMovies = function(req, res) {
+  handleGetAllMovies(req, res);
+}
+app.get('/movies', handleGetMovies)
 
 
 app.listen(8080, () => {
