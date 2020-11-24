04624d7b1ce14b198a6f84d59f920325d7c8a6a4
rhammock1 <rhamm1027@gmail.com>
2020-11-24 14:55:54 -0500

    Added array of movies

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const helmet = require('helmet')
 const app = express();
 
 app.use(cors())
 app.use(morgan("combined"));
-
+app.use(helmet())
 
 
 app.listen(8080, () => {
